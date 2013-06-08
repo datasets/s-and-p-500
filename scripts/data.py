ef945e7691e8ca49bf950094936a50a4756f0555
Rufus Pollock <rufus.pollock@okfn.org>
2013-06-08 19:53:22 +0100

    [scripts][m]: refactor script to produce cleaner data and be simpler.
    * round all numbers to 2 digits (as it is in original spreadsheet) to avoid issue that o/w we get 9 dec places and data changes all the time spuriously due to floating point (or just excel - hard to tell)
    * simplify code and explicitly set headings

--- scripts/data.py
+++ scripts/data.py
@@ -1,38 +1,76 @@
 url = 'http://www.econ.yale.edu/~shiller/data/ie_data.xls'
-cache = 'cache'
+cachepath = 'cache/shiller.xls'
 
+import urllib
 import datautil
 import datautil.tabular
-cache = datautil.Cache(cache)
 out_filepath = 'data/data.csv'
 
-class Extractor(object):
-    def execute(self):
-        fp = cache.retrieve(url, overwrite=True)
-        reader = datautil.tabular.XlsReader(fp)
-        # print reader.info()
-        tabdata = reader.read()
-        # clean up data
-        data = tabdata.data
-        # headings spread across rows 2-8
-        headings = zip(*data[1:8])
-        tabdata.header = [ ' '.join(cols).strip() for cols in headings ]
-        data = tabdata.data[8:-1]
-        transposed = zip(*data)
-        # get rid of odd date e.g. 1871.01 and replace with date fraction
-        fraction = transposed[5]
-        del transposed[5]
-        transposed[0] = fraction
-        tabdata.data = zip(*transposed)
-        del tabdata.header[5]
-        del tabdata.header[0]
-        tabdata.header.insert(0, 'Date')
-        tabdata.header[1] = 'Price'
-        tabdata.header[-1] = 'P/E10'
-        writer = datautil.tabular.CsvWriter()
-        writer.write(tabdata, open(out_filepath, 'w'))
+def execute():
+    fp = retrieve()
+    out = extract(fp)
+    save(out, out_filepath)
 
+def retrieve():
+    urllib.urlretrieve(url, cachepath)
+    return cachepath
+
+def extract(fp):
+    reader = datautil.tabular.XlsReader(fp)
+    # print reader.info()
+    tabdata = reader.read()
+    out = datautil.tabular.TabularData()
+
+    # headings spread across rows 2-8
+    out.header = [
+        'Date',
+        'SP500',
+        'Dividend',
+        'Earnings',
+        'Consumer Price Index',
+        'Long Interest Rate',
+        'Real Price',
+        'Real Dividend',
+        'Real Earnings',
+        'P/E10'
+        ]
+
+    # first 8 rows are headers, last 2 rows are footnotes so trim them
+    data = tabdata.data[8:-2]
+    transposed = zip(*data)
+    # fix dates
+    # delete "date fraction" column
+    del transposed[5]
+    transposed[0] = [ _fixdates(val) for val in transposed[0] ]
+    for idx, row in enumerate(transposed[1:]):
+        row = [ _fixup(val) for val in row ]
+        transposed[idx+1] = row
+    
+    out.data = zip(*transposed)
+    return out
+
+def save(tabdata, out_filepath):
+    writer = datautil.tabular.CsvWriter()
+    writer.write(tabdata, open(out_filepath, 'w'))
+
+def _fixup(val):
+    if val == 'NA':
+        return ''
+    elif val == '':
+        return ''
+    return round(val, 2)
+
+def _fixdates(val):
+    # 1879.03 = march, 1879.1 = october
+    val = round(val,2)
+    year = int(val)
+    # add 0.0001 to ensure we don't have problems re rounding
+    month = int(0.1 + (val - year) * 100)
+    out = str(year) + '-' + str(month).zfill(2) + '-01'
+    return out
 
 if __name__ == '__main__':
-    Extractor().execute()
+    # out = extract(cachepath)
+    # save(out, out_filepath)
+    execute()
 
